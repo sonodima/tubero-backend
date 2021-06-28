0d0327e54f5df1076a0e7d96effb170387d32e69
sonodima <sonodimaa@icloud.com>
2021-06-28 00:57:02 -0700

    added key-less youtube search

--- src/types/SearchData.ts
+++ src/types/SearchData.ts
@@ -1,14 +1,14 @@
 type SearchData = {
-  title?: string | undefined;
-  artist?: string | undefined;
-  album?: string | undefined;
-  genre?: string | undefined;
-  trackNumber?: number | undefined;
-  tracksCount?: number | undefined;
-  discNumber?: number | undefined;
-  discsCount?: number | undefined;
-  releaseDate?: string | undefined;
-  cover?: string | undefined;
+  title?: string;
+  artist?: string;
+  album?: string;
+  genre?: string;
+  trackNumber?: number;
+  tracksCount?: number;
+  discNumber?: number;
+  discsCount?: number;
+  releaseDate?: string;
+  cover?: string;
 };
 
 export default SearchData;
